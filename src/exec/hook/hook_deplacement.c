0eae455da53652348be8c45106263e35275a3569
Jules Laisne <jlaisne@z2r7p8.42lyon.fr>
2023-06-02 15:20:14 +0200

    retrun

--- src/exec/hook/hook_deplacement.c
+++ src/exec/hook/hook_deplacement.c
@@ -6,7 +6,7 @@
 /*   By: jlaisne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 17:52:57 by vgonnot           #+#    #+#             */
-/*   Updated: 2023/06/02 15:14:47 by jlaisne          ###   ########.fr       */
+/*   Updated: 2023/06/02 15:19:56 by jlaisne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,6 @@ void	horizontal_movement(t_exec *exec, int keycode, int *x, int *y)
 		// if (exec->angle > 2 * PI)
 		// 	exec->angle = 0;
 	}
-	return (0);
 }
 
 int    check_in_map(float x, float y, t_exec *exec, int keycode)
